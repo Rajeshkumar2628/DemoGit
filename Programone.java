5560ccc763f555a6495bf48ab1beb4b2d0b49db3
N Rajesh kumar <86413492+Rajeshkumar2628@users.noreply.github.com>
2025-12-31 16:29:51 +0530

    Selenium Workspace

---
+++ Programone.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Programone {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
+		WebDriver d=new ChromeDriver();
+		d.get("https://www.google.com");
+	}
+
+}
